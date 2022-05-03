b160ac7f572fe03b50d1f290d2078a84394c192f
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-05-03 22:27:17 +0300

    test1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+function addGalleryItemsImgs(obj) {
+    const gallery = obj.map(createGalleryElement).join('');
+
+    galleryRef.innerHTML = gallery;
+    
+}
+addGalleryItemsImgs(galleryItems);
+
+function createGalleryElement({preview, original, description}) {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"></a>`
+};
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 20, });
