7933e248ac69be2c7ab1c48abf219ed96b25b3c3
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-05-05 16:59:35 +0300

    домашка)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+  import throttle from "lodash.throttle";
 
+    const iframe = document.querySelector('iframe');
+    
+    const player = new Vimeo.Player(iframe);
+    const STORAGE_KEY_PLAYTIME = "videoplayer-current-time";
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+
+    function onPlay(data){
+        const timePlayed = JSON.stringify(data.seconds);
+        localStorage.setItem(STORAGE_KEY_PLAYTIME, timePlayed);
+    };
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    setCurrentTime();
+    function setCurrentTime(){
+        const savedTime = localStorage.getItem(STORAGE_KEY_PLAYTIME);
+        if(savedTime){
+            const convertSavedTime = JSON.parse(savedTime);
+            player.setCurrentTime(convertSavedTime);
+        }
+    }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    inputEmail: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+    submitBtn: document.querySelector('.feedback-form button')
+};
+
+const STORAGE_KEY = "feedback-form-state";
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInputChange, 500));
+
+fillerFormFields();
+    
+function onInputChange(evt){
+    const objSavedData = {};
+    objSavedData.email = refs.inputEmail.value;
+    objSavedData.message = refs.textarea.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(objSavedData));
+};
+
+function onFormSubmit(evt){
+    evt.preventDefault();
+    
+    const email = evt.currentTarget.elements.email.value;
+    const message = evt.currentTarget.elements.message.value;
+
+    if(email === '' || message === ''){
+        return
+    }
+
+    console.log({email, message});
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function fillerFormFields(){
+    const storageData = localStorage.getItem(STORAGE_KEY);
+    const convertStorageData = JSON.parse(storageData);
+
+    if (storageData){
+        refs.inputEmail.value = convertStorageData.email;
+        refs.textarea.value = convertStorageData.message;
+    }
+};
